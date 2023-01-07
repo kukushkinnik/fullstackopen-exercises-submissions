f2988617e2c3f2b7c086f4728f3ebbddef860f61
Nikita <kukushkinnika@gmail.com>
2023-01-07 14:00:03 -0500

    Finished 4.1-4.2 exercises and updated README

---
+++ part4/bloglist/app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const { info, error } = require("./utils/logger");
+const { MONGODB_URI } = require("./utils/config");
+const blogRouter = require("./controllers/blog");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
+
+info(`Connecting to MongoDB ${MONGODB_URI}`);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => info("Connected"))
+  .catch((er) => error(er.message));
+
+app.use(cors());
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+
+module.exports = app;
---
+++ part4/bloglist/index.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require("./app");
+const { PORT } = require("./utils/config");
+const { info } = require("./utils/logger");
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  info(`Server is running on ${PORT}`);
+});
